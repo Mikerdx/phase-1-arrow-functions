637c05a17124c732dbe892960e2d54535073c31d
mike <mike@DESKTOP-2JKV4QP.>
2025-03-05 08:38:42 +0300

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (a, b) => a / b;
+console.log(divide(2000, 100)); 
+
+const square = x => x ** 2; 
+console.log(square(5));
+
+const add = (a, b) => a + b;
+console.log(add(3, 7)); 
+
+const numbers = [1, 2, 3, 4];
+const squares = numbers.map(square); 
+console.log(squares); 
